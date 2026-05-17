4fc7f11fdb87b3b0db0b6796bd0752eedf11fdc3
waveeyz <djeslykeuren12@gmail.com>
2026-05-17 10:49:18 +0200

    Delete app directory

--- app/api/admin/route.ts
+++
@@ -1,7 +0,0 @@
-
-import { NextResponse } from 'next/server';
-import {supabaseAdmin} from '../../../lib/supabase';
-
-export async function POST(req: Request) {
-  return NextResponse.json({ success: true });
-}
